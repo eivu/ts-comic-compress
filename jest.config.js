fb4807871e061bbdc442d76ccb8280e8f5cad984
Rob Jenkins <384403+dabobert@users.noreply.github.com>
2026-01-30 17:07:04 -0500

    excluding files from coverage

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,12 @@ module.exports = {
   testEnvironment: "node",
   roots: ["<rootDir>/src"],
   testMatch: ["**/__tests__/**/*.test.ts"],
-  collectCoverageFrom: ["src/**/*.ts", "!src/**/*.d.ts", "!src/index.ts"],
+  collectCoverageFrom: [
+    "src/**/*.ts",
+    "!src/**/*.d.ts",
+    "!src/index.ts",
+    "!src/__tests__/**",
+  ],
   coverageDirectory: "coverage",
   coverageReporters: ["text", "lcov", "html"],
   moduleFileExtensions: ["ts", "js", "json"],
